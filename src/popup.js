7a393202cf096acd60b7733ce0804469d8a2975c
Matheus R. Brunelli <matheus.brunelli@gmail.com>
2021-04-01 21:07:42 -0300

    refactor: separate logic into separate functions

--- src/popup.js
+++ src/popup.js
@@ -8,42 +8,53 @@ const baseUrl = "https://pt.wikipedia.org/api/rest_v1/page/summary/";
 const onSubmit = (e) => {
   e.preventDefault();
   const { input } = form;
+  const text = input.value.toString();
+  const formatedText = replaceWhitespaceToUnderlined(text);
+  formatedText.length > 0 && searchText(formatedText);
+};
 
-  let text = input.value.toString();
-  text = replaceWhitespaceToUnderlined(text);
-
-  text.length > 0 && searchText(text);
+const replaceWhitespaceToUnderlined = (value) => {
+  return value.replace(/( )+/g, "_");
 };
 
 const searchText = async (inputText) => {
   const res = await fetch(baseUrl + inputText);
   const data = await res.json();
-
-  renderHtml(data, inputText);
+  renderHtml(data);
 };
 
-const renderHtml = (dataJson, inputText) => {
-  const { extract_html, thumbnail, content_urls } = dataJson;
-
-  extract_html
-    ? (
-      message.innerHTML = extract_html,
-        link.setAttribute("href", content_urls?.desktop?.page),
-        link.classList.remove("hidden"),
-        thumbnail?.source
-          ? img.setAttribute("src", thumbnail.source)
-          : img.removeAttribute("src")
-    )
-    : (
-      message.innerHTML =
-        `<p>Nenhuma correspondencia para <b>${inputText}</b>...</p>`,
-        link.classList.add("hidden"),
-        img.removeAttribute("src")
-    );
+const setOrRemoveLinkHref = (dataJson) => {
+  const { content_urls } = dataJson;
+  if (content_urls?.desktop) {
+    link.setAttribute("href", content_urls.desktop?.page);
+    link.classList.remove("hidden");
+  } else {
+    link.removeAttribute("href");
+    link.classList.add("hidden");
+  }
 };
 
-const replaceWhitespaceToUnderlined = (value) => {
-  return value.replace(/( )+/g, '_');
-}
+const setOrRemoveThumbnailSrc = (dataJson) => {
+  const { thumbnail } = dataJson;
+  thumbnail?.source
+    ? img.setAttribute("src", thumbnail.source)
+    : img.removeAttribute("src");
+};
+
+const setInnerHtmlContent = (dataJson) => {
+  const { extract_html } = dataJson;
+  if (extract_html) {
+    message.innerHTML = extract_html;
+  } else {
+    message.innerHTML =
+      `<p>Nenhuma correspondência para <b>${input.value}</b>...</p>`;
+  }
+};
+
+const renderHtml = (dataJson) => {
+  setInnerHtmlContent(dataJson);
+  setOrRemoveLinkHref(dataJson);
+  setOrRemoveThumbnailSrc(dataJson);
+};
 
 form.addEventListener("submit", onSubmit);
